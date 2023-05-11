c1a2a30e8214b198e0ddafd85697378e9d04c008
MariamSaidGaber <saidmariam463@gmail.com>
2023-05-11 22:20:42 +0300

    4 arrows

--- 01-Python/03-Projects&Tasks/06-Arrow/arrow.py
+++ 01-Python/03-Projects&Tasks/06-Arrow/arrow.py
@@ -1,21 +1,41 @@
 ###################################################3
-# pyramid upper side
+
+
+def Arrow_Right(lines):
+    #Arrow upper side
+    list1 =[]
+    for i in range(0,lines):
+        list1.append( (" "*lines)  + ("*"*i) )
+    print("\n".join(list1))      # join fun: to print list element in separate way 
+
+    #######################
+    # Middle side
+    print(("*"*lines*2))
+    # Arrow lower side
+
+    list2 =[]
+    for count in range(0,lines):
+        list2.append( (" "*lines) + ("*"*(lines-count-1)) )
+    print("\n".join(list2))      # join fun: to print list element in separate way 
+################################################################3    
+def Arrow_Left(lines):
+    #Arrow upper side
+    list1 =[]
+    for i in range(0,lines):
+        list1.append( (" "*(lines-i) ) + ("*"*i) )
+    print("\n".join(list1))      # join fun: to print list element in separate way 
+
+    #######################
+    # Middle side
+    print(("*"*lines*2))
+    #####################
+    # Arrow lower side
+    list2 =[]
+    for count in range(0,lines):
+        list2.append((" "*(count+1)) + ("*"*(lines-count-1)) )
+    print("\n".join(list2))      # join fun: to print list element in separate way     
+################## 
+    
 Num_lines = int(input("Enter num of lines: "))
-
-list1 =[]
-for i in range(0,Num_lines):
-    list1.append( (" "*Num_lines)  + ("*"*i) )
-print("\n".join(list1))      # join fun: to print list element in separate way 
-
-
-#######################
-# Pyramid middle
-print(("*"*Num_lines*2))
-
-# Pyramid lower side
-
-list2 =[]
-for count in range(0,Num_lines):
-    list2.append( (" "*Num_lines) + ("*"*(Num_lines-count-1)) )
-print("\n".join(list2))      # join fun: to print list element in separate way 
-##################################################  
+Arrow_Right(Num_lines)   
+Arrow_Left(Num_lines)
