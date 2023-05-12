1dcabc975731a42b46b84b0c3c06b1a8747f34b2
MariamSaidGaber <saidmariam463@gmail.com>
2023-05-13 01:18:01 +0300

    Edit

--- 01-Python/03-Projects&Tasks/07-Calculator/calc.py
+++ 01-Python/03-Projects&Tasks/07-Calculator/calc.py
@@ -1,4 +1,4 @@
-import sys
+
 
 class Calc():
     def __init__(self,total =0,res =0):
@@ -42,45 +42,48 @@ class Calc():
         calculate = "1"
         while calculate != "0":
             num1 = int (input("PLease Enter Num1: "))
-            try:
-                choice = input("Choose the operator: +,-,*,/,%,^: ")
+
+            choice = input("Choose the operator: +, -, *, /, %, ^ : ")
+   
+            while choice not in ("+,-,*,/,%,^"):
+                choice = input("Please choose +, -, *, /, %, ^ ")
+                    
+            if choice == "+":
+                    
+                num2 = int (input("Please Enter Num2: ")) 
+                print(num1 ,"+", num2, "=",Calc.Add(self,num1,num2) ) 
                 
-            except:
-                print("Please choose +,-,*,/,%,^")
-            else:  
-                if choice == "+":
-                    
-                    num2 = int (input("Please Enter Num2: ")) 
-                    print(num1 ,"+", num2, "=",Calc.Add(self,num1,num2) ) 
-                    
-                elif choice == "-":
-                   
-                    num2 = int (input("Enter Num2: ")) 
-                    print(num1 ,"-", num2, "=",Calc.Sub(self,num1,num2) ) 
-                    
-                elif choice == "*":
-                 
-                    num2 = int (input("Enter Num2: ")) 			
-                    print(num1 ,"*", num2, "=",Calc.Mul(self,num1,num2) ) 
-                    
-                elif choice == "/":
-                   
-                    num2 = int (input("Enter Num2: ")) 
-                    print(num1 ,"/", num2, "=",Calc.Div(self,num1,num2) )
-                    
-                elif choice == "%":
-                  
-                    num2 = int (input("Enter Num2: ")) 
-                    print(num1 ,"%", num2, "=",Calc.Div_Remen(self,num1,num2) ) 
-                    
-                elif choice == "^":
-                    
-                    num2 = int (input("Enter Num2: ")) 
-                    print(num1 ,"^", num2, "=",Calc.Power(self,num1,num2) ) 
-                    
-                calculate = input("If you want to exit press 0: ")
-                if calculate == "0":
-                    break
+            elif choice == "-":
+                
+                num2 = int (input("Enter Num2: ")) 
+                print(num1 ,"-", num2, "=",Calc.Sub(self,num1,num2) ) 
+                
+            elif choice == "*":
+                
+                num2 = int (input("Enter Num2: ")) 			
+                print(num1 ,"*", num2, "=",Calc.Mul(self,num1,num2) ) 
+                
+            elif choice == "/":
+                num2 = int (input("Enter Num2: "))
+                while num2 == 0:
+                        num2 = int (input("PLease Enter Valid Num "))
+                
+                print(num1 ,"/", num2, "=",Calc.Div(self,num1,num2) )
+                
+            elif choice == "%":
+                
+                num2 = int (input("Enter Num2: ")) 
+                print(num1 ,"%", num2, "=",Calc.Div_Remen(self,num1,num2) ) 
+                
+            elif choice == "^":
+                
+                num2 = int (input("Enter Num2: ")) 
+                print(num1 ,"^", num2, "=",Calc.Power(self,num1,num2) ) 
+                
+            calculate = input("If you want to exit press 0: ")
+            if calculate == "0":
+                print ("Thank you")
+                break
                     
 
               
